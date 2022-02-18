afc5152c64b3bd57e924ecb27f0251e611af635a
rusty <adalbertog20>
2022-02-18 14:27:31 -0700

    Ahora si da display y habre terminales

--- tilin.c
+++ tilin.c
@@ -1,56 +1,62 @@
 #include <X11/X.h>
-#include <stdio.h>
-#include <stdbool.h>
 #include <X11/Xlib.h>
-#define MAX(a, b) ((a)> (b) ? (a) : (b))
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    //Declaring some objects
-    XEvent event;
-    XWindowAttributes attributes;
-    XButtonEvent start;
-    Window focus;
-    int revert;
-    Display *display = NULL;
+int main(void) {
+  Display *display;
+  Window win_foc;
+  XWindowAttributes attr;
+  XButtonEvent start;
+  XEvent event;
+  int revert;
 
-    if(!(display == XOpenDisplay(NULL)))
-    {
-        return 1;
+  if (!(display = (XOpenDisplay(0x0))))
+    return 1;
+
+  XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("q")), Mod4Mask,
+           DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
+
+  XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("Return")),
+           Mod4Mask, DefaultRootWindow(display), True, GrabModeAsync,
+           GrabModeAsync);
+
+  XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("BackSpace")),
+           Mod4Mask, DefaultRootWindow(display), True, GrabModeAsync,
+           GrabModeAsync);
+
+  XGrabButton(display, 1, Mod4Mask, DefaultRootWindow(display), True,
+              ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
+              GrabModeAsync, GrabModeAsync, None, None);
+  XGrabButton(display, 3, Mod4Mask, DefaultRootWindow(display), True,
+              ButtonPressMask | ButtonReleaseMask | PointerMotionMask,
+              GrabModeAsync, GrabModeAsync, None, None);
+
+  start.subwindow = None;
+
+  for (;;) {
+    XNextEvent(display, &event);
+    XGetInputFocus(display, &win_foc, &revert);
+    XSync(display, False);
+    XSetInputFocus(display, PointerRoot, RevertToPointerRoot, CurrentTime);
+
+    if (event.type == KeyPress && event.xbutton.subwindow != None) {
+
+      if (event.xkey.keycode ==
+          XKeysymToKeycode(display, XStringToKeysym("q"))) {
+        XDestroyWindow(display, win_foc);
+      }
     }
 
-    //Mappings
-    //Kill Window
-    XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("q")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
-    //Open Terminal
-    XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("Return")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
-    //Capture mouse buttons
-    XGrabButton(display, 1, Mod1Mask, DefaultRootWindow(display), True,
-        ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(display, 3, Mod1Mask, DefaultRootWindow(display), True,
-        ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    start.subwindow = None;
-    while(true)
-    {
-        //Initialize stuff
-        XNextEvent(display, &event);
-        XGetInputFocus(display, &focus, &revert);
-        XSync(display, false);
-        XSetInputFocus(display, PointerRoot, RevertToPointerRoot, CurrentTime);
-
-        //Close the window
-        if(event.type == XKeysymToKeycode(display , XStringToKeysym("q")))
-        {
-            XDestroyWindow(display, focus);
-        }
-        if(event.type == KeyPress)
-        {
-            if(event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("Return")))
-            {
-                system("st &");
-            }
-        }
-
+    if (event.type == KeyPress) {
+      if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("Return"))) {
+        system("st &");
+      }
+      if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("p"))) {
+        system("rofi -show drun");
+      } 
+      if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("BackSpace"))) {
+        XCloseDisplay(display);
+      }
     }
-    return 0;
+  }
 }
